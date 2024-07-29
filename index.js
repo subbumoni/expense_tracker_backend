8a9567400c8ac271d153a1ec61e17626e12b55b7
subbumoni <subramanianbeee@gmail.com>
2024-07-29 11:41:21 +0530

    Added Env configs

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require("express")
-const http_server = express()
+const express = require("express");
+const http_server = express();
 
-const PORT = 5000;
-const HOSTNAME = 'localhost';
+
+
+//CONFIGUR  DOTENV
+require("dotenv").config();
+const PORT = process.env.PORT;
+const HOSTNAME = process.env.HOSTNAME;
 
 http_server.listen(PORT, HOSTNAME, () => {
-    console.log(`server start at http://${HOSTNAME}:${PORT}`);
-})
+  console.log(`server start at http://${HOSTNAME}:${PORT}`);
+});
 
-http_server.get("/", (req,res) => {
-    res.status(200).json({
-       message:"Server started successfully"
-   })
-})
+http_server.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server started successfully",
+  });
+});
